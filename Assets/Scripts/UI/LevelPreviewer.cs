3df2c1e5f44f4fd711b0feccc52541176480c975
刘浩然 <1042742166@qq.com>
2023-05-08 02:25:11 +0800

    add arrow UI for LevelSelectMenu

--- Assets/Scripts/UI/LevelPreviewer.cs
+++ Assets/Scripts/UI/LevelPreviewer.cs
@@ -9,6 +9,8 @@ public class LevelPreviewer : MonoBehaviour
     public Text curLevelName;
     public List<Sprite> levelOverviewSprites;
     public Image curLevelOverview;
+    public Image leftArrow;
+    public Image rightArrow;
     void Start()
     {
         //levelOverviewSprites = new List<Sprite>(GlobalTerminal.Instance.Global_MaxLevelIndex);
@@ -33,5 +35,21 @@ public class LevelPreviewer : MonoBehaviour
     {
         curLevelName.text = (GlobalTerminal.Instance.Global_LevelIndex / 10).ToString() + (GlobalTerminal.Instance.Global_LevelIndex % 10).ToString();
         curLevelOverview.sprite = levelOverviewSprites[GlobalTerminal.Instance.Global_LevelIndex - 1];
+        if (GlobalTerminal.Instance.Global_LevelIndex == 1)
+        {
+            leftArrow.color = new Color(1, 1, 1, 0.5f);
+        }
+        else
+        {
+            leftArrow.color = new Color(1, 1, 1, 1);
+        }
+        if (GlobalTerminal.Instance.Global_LevelIndex == GlobalTerminal.Instance.Global_UnlockedLevelIndex)
+        {
+            rightArrow.color = new Color(1, 1, 1, 0.5f);
+        }
+        else
+        {
+            rightArrow.color = new Color(1, 1, 1, 1);
+        }
     }
 }
